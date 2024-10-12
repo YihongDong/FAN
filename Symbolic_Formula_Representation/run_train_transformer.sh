f0f0e061aed67611bb56eada3399a0cd95b183fc
YihongDong <37007374+YihongDong@users.noreply.github.com>
2024-10-13 01:02:39 +0800

    Update Symbolic_Formula_Representation

--- Symbolic_Formula_Representation/run_train_transformer.sh
+++ Symbolic_Formula_Representation/run_train_transformer.sh
@@ -2,7 +2,7 @@ dataset_ids=(0 1 2 3 4)
 
 for dataset_id in ${dataset_ids[@]}
 do
-    CUDA_VISIBLE_DEVICES=5 python train_transformer.py \
+    CUDA_VISIBLE_DEVICES=0 python train_transformer.py \
         --dataset_id $dataset_id \
         --dataset_dir dataset \
         --save_dir transformer_checkpoint
